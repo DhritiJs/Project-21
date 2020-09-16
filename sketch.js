3dab008116c8d68c7afded29c4c1b20eb33d1435
DhritiJs <70759231+DhritiJs@users.noreply.github.com>
2020-09-16 16:59:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var bullet,wall;
+var thickness,speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83)
+  bullet=createSprite(50,200,40,10);
+  bullet.shapeColor="white";
+  bullet.velocityX=speed;
+  
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor="grey";
+}
+
+function draw() {
+  background(0); 
+ 
+  if(hasCollided(bullet,wall)){
+ bullet.velocityX=0;
+ var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+ if(damage>10){
+   wall.shapeColor=color(255,0,0);
+ }
+
+ if(damage<10){
+   wall.shapeColor=color(0,255,0);
+ }
+
+
+
+  }
+
+    bullet.depth=wall.depth;
+    bullet.depth=bullet.depth+1;
+
+
+
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+bulletRightEdge=lbullet.x+lbullet.width
+wallLeftEdge=lwall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true;
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
